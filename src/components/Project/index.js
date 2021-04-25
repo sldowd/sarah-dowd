7febac9de299942583a5aab00a59d4fad3ffcb05
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-04-25 08:47:34 -0400

    added projects map and favicon

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
-function Project() {
+
+
+function Project(props) {
+
+    const currentProject = [
+        {
+            name: "Paranormal Tourism Database",
+            imgSrc: "./assets/PTDB-screenshot.png",
+            deployedUrl: "http://paranormal-tourism.herokuapp.com/",
+            githubUrl: "https://github.com/RaeStichter/paranormal-tourism"
+        },
+        {
+            name: "Dungeon Masters Guide",
+            imgSrc: "./assets/DMG-screenshot.png",
+            deployedUrl: "https://dungeon-masters-guide.herokuapp.com/",
+            githubUrl: "https://github.com/RaeStichter/initiate-dnd"
+        },
+]
+
     return (
-        <h1>Project Section</h1>
+        <section>
+            {currentProject.map((project) =>
+                <div key={project}>
+                    <h3>{project.name}</h3>
+                    <img src={project.imgSrc} alt="Screenshot of project"/>
+                    <a href={project.deployedUrl}>Deployed Project | </a>
+                    <a href={project.githubUrl}>Github Repository</a>
+                </div>
+            )}
+        </section>
     );
 }
 
