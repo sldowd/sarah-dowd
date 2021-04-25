c40359ff55791e846d6226a3b1b7c140d1f75939
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-04-25 07:28:35 -0400

    nav added, pivoting to projects

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Header() {
     return (
         <header>
-
+            <Navbar fixed="top">
+                <Navbar.Brand href="#home">Sarah Dowd</Navbar.Brand>
+                <Nav className="justify-content-end">
+                    <Nav.Item>
+                        <Nav.Link href="/About">About</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/Portfolio">Portfolio</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/Resume">Resume</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/Contact">Contact</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar>
         </header>
     );
 }
