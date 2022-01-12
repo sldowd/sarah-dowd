3307a984b2ef06210d85301f93fedaec32f6df12
Sarah Dowd <sarahlynnedowd@gmail.com>
2022-01-11 19:13:53 -0500

    updated about section with brand statement

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function About() {
     return (
-        <h1>Hi, I'm Sarah! This portfolio is a work in progress.</h1>
+        <div>
+        <h1>Sarah Dowd</h1>
+        <h7>Full-stack web developer with a passion for community and lifelong learning. Skilled in HTML, CSS, JavaScript and Node.js, 
+            with some experience with Sass and SCSS. I received my certificate in full-stack web development from the University of 
+            Pennsylvania. Before I found my passion for coding, I studied health and wellness and became a certified health coach while 
+            working multiple jobs in the restaurant industry. My experience has taught me the importance of balance, attention to detail, 
+            and prioritizing multiple important tasks at once. While working as a food cost manager for several years, I developed 
+            analytical skills that sharpened my problem-solving skills. My biggest takeaway from learning web development was not how 
+            to know all the answers but how to find them. I learned there is great value in working as part of a team as a developer. 
+            I also worked successfully on multiple projects as part of a remote team. Passionate about working with a company where I 
+            can sharpen my skills and grow as a developer. My ultimate goal is to one day be able to offer web development services to 
+            local businesses in my community to help them stay competitive in an increasingly digital world. </h7>
+        </div>
     )
 }
 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -18,6 +18,18 @@ function Project(props) {
             deployedUrl: "https://dungeon-masters-guide.herokuapp.com/",
             githubUrl: "https://github.com/RaeStichter/initiate-dnd"
         },
+        {
+            name: "Project Tracker",
+            imgSrc: "./assets/project-tracker-image.png",
+            deployedUrl: "https://sldowd.github.io/kanban-board/",
+            githubUrl: "https://github.com/sldowd/kanban-board"
+        },
+        {
+            name: "Dungeon Masters Guide",
+            imgSrc: "./assets/DMG-screenshot.png",
+            deployedUrl: "https://dungeon-masters-guide.herokuapp.com/",
+            githubUrl: "https://github.com/RaeStichter/initiate-dnd"
+        }
 ]
 
     return (
@@ -25,7 +37,7 @@ function Project(props) {
             
             {currentProject.map((project) =>
                 <div className="my-2 flex-row space-between" key={project}>
-                    <Card className="flex-row spacebetween">
+                    <Card className="flex-row space-between">
                     <Card.Title title={project.name}></Card.Title>
                     <img className="img" src={project.imgSrc} alt="Screenshot of project"/>
                         <Card.Body>
